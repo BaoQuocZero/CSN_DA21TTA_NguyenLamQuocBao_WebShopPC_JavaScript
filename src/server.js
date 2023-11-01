3743632843d8176a7d23568e2560a82441c5d518
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-01 19:40:54 +0700

    up

--- src/server.js
+++ src/server.js
@@ -1,18 +1,24 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 const port = 3000
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/abc', (req, res) => {
-    res.send('Check ABC')
-  })
+  res.send('Check ABC')
+})
 
-  app.get('/hoidanit', (req, res) => {
-    res.send('<h1>Hoi dan IT</h1>')
-  })
+app.get('/hoidanit', (req, res) => {
+  //res.send('<h1>Hoi dan IT</h1>')
+  res.render('sample.ejs')
+})
 
 
 app.listen(port, () => {
