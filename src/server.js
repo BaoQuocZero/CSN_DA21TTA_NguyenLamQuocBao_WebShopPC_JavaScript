25f36192ff3d5e29725a21e7d423ec4f554af3ec
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-06 20:28:48 +0700

    30

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const express = require('express')//commonjs
 //const path = require('path')//commonjs
 const confitViewEngine = require('./config/viewEngine');
 const webRouter = require('./routers/web')
-
-//console.log(">>>Check env: ", process.env);
+// get the client
+const mysql = require('mysql2');
 
 const app = express()//add express
 const port = process.env.PORT || 8888;//port => hardcore .uat .prod
@@ -15,6 +15,14 @@ confitViewEngine(app);
 
 //khai báo route
 app.use('/', webRouter);
+
+//Test connection
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test'
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
