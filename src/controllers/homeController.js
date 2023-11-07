ae2618b795682f2314ccf8d483c582cbae5ece01
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-07 21:15:44 +0700

    39

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -12,7 +12,7 @@ const hotdanit = (req, res) => {
     res.render('sample.ejs')
 }
 
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
     console.log('>>>Check req.body', req.body)
     let email = req.body.email
     let name = req.body.myname
@@ -22,16 +22,26 @@ const postCreateUser = (req, res) => {
 
     console.log('email=', email, ' name = ', name, ' city= ', city)
 
-    connection.query(
+    // connection.query(
+    //     `INSERT INTO Users (email, name, city) VALUE(?, ?, ?)`,
+    //     [email, name, city],
+    //     function (err, results) {
+    //         console.log(results);
+    //         res.send('Create user succeed');
+    //     }
+    // );
+
+    let [results, fields] = await connection.query(
         `INSERT INTO Users (email, name, city) VALUE(?, ?, ?)`,
         [email, name, city],
-        function (err, results) {
-            console.log(results);
-            res.send('Create user succeed');
-        }
     );
+    res.send('Create user succeed');
+    // const [results, fields] = await connection.query('SELECT * FROM Users');
+}
+const getCreatePage = (req, res) => {
+    res.render('create.ejs')
 }
 
 module.exports = {
-    getHomepage, getABC, hotdanit, postCreateUser
+    getHomepage, getABC, hotdanit, postCreateUser, getCreatePage
 }
--- src/routers/web.js
+++ src/routers/web.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getHomepage, getABC, hotdanit, postCreateUser } = require('../controllers/homeController');
+const { getHomepage, getABC, hotdanit, postCreateUser, getCreatePage } = require('../controllers/homeController');
 const router = express.Router();
 
 router.get('/', (getHomepage))
 router.get('/abc', getABC)
 router.get('/hoidanit', hotdanit);
+router.get('/create', getCreatePage);
+
 router.post('/create-user', postCreateUser)
 
 module.exports = router;//exports default
