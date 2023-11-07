3818f34c170109859c43c1ef18aaf5f17173dd48
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-07 14:40:20 +0700

    31

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const express = require('express')//commonjs
 //const path = require('path')//commonjs
 const confitViewEngine = require('./config/viewEngine');
 const webRouter = require('./routers/web')
+const connection = require('./config/database');
 // get the client
-const mysql = require('mysql2');
 
 const app = express()//add express
 const port = process.env.PORT || 8888;//port => hardcore .uat .prod
@@ -16,22 +16,11 @@ confitViewEngine(app);
 //khai báo route
 app.use('/', webRouter);
 
-//Test connection
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307,//default 3306
-  user: 'root',//default emty
-  password: '123456',
-  database: 'hoidanit'
-});
-
 // simple query
 connection.query(
   'SELECT * FROM Users',
   function (err, results, fields) {
     console.log('>>>>results= ', results); // results contains rows returned by server
-    console.log('>>>>fields= ', fields); // fields contains extra meta data about results, if available
   }
 );
 
