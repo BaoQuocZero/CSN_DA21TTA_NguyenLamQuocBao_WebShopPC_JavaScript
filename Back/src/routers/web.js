e0998897e07e32dbd10e59b6291408a8360eb565
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-13 20:17:52 +0700

    Up Fron

--- Back/src/routers/web.js
+++ Back/src/routers/web.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { getHomepage, getABC, hotdanit, postCreateUser, getCreatePage, getUpdatePage, postUpdateUser,
+const { getHomepage, getNV, getSP, postCreateUser, getCreatePage, getUpdatePage, postUpdateUser,
     postDeleteUser, postHandlRemoveUser
 } = require('../controllers/homeController');
 const router = express.Router();
 
 router.get('/', (getHomepage))
-router.get('/abc', getABC)
-router.get('/hoidanit', hotdanit);
+router.get('/nv', getNV)
+router.get('/sanpham', getSP);
 router.get('/create', getCreatePage);
 router.get('/update/:id', getUpdatePage);
 
---
+++ Fron/src/views/Example/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello my Component Hoi dan it</div>
+        )
+    }
+}
+
+export default MyComponent;
