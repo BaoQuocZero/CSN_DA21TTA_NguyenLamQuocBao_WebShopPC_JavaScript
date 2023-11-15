d7865aa63b8f948c03ce8e555c256298cf48b4f0
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-15 19:08:00 +0700

    13

--- Fron/src/views/Example/ChildComponent.js
+++ Fron/src/views/Example/ChildComponent.js
@@ -1,22 +1,41 @@
 import React from "react";
 
 class ChildComponent extends React.Component {
+    state = {
+        showJobs: false
+    }
+
+    handleShowHide = () => {
+        this.setState({
+            showJobs: !this.state.showJobs
+        })
+    }
     render() {
-        console.log(">>>Check props: ", this.props)
-        let { name, age, address, arrJobs } = this.props
+        let { arrJobs } = this.props
+        let { showJobs } = this.state;
+
         return (
             <>
-                <div className="job-lists">
-                    {
-                        arrJobs.map((item, index) => {
-                            return (
-                                <div key={item.id}>
-                                    {item.title} - {item.salary}
-                                </div>
-                            )
-                        })
-                    }
-                </div >
+                {showJobs === false ?
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>Show</button>
+                    </div>
+                    :
+                    <>
+                        <div className="job-lists">
+                            {
+                                arrJobs.map((item, index) => {
+                                    return (
+                                        <div key={item.id}>
+                                            {item.title} - {item.salary}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div >
+                        <div><button onClick={() => this.handleShowHide()}>Hide</button></div>
+                    </>
+                }
             </>
         )
     }
