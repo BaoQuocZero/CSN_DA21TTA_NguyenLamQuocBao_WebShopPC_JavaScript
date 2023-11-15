ef4b5d6537514079a2b1e57dc5f397ba45e5b18f
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-15 18:40:04 +0700

    12

--- Fron/src/views/Example/ChildComponent.js
+++ Fron/src/views/Example/ChildComponent.js
@@ -1,33 +1,6 @@
 import React from "react";
 
 class ChildComponent extends React.Component {
-    /*
-    JFX => return 1 block hay 1 div
-    Nếu muốn nhiều hơn 1 div ta dùng <> div1 div2 </>
-    */
-    state = {
-        firstName: "",
-        lastName: ""
-    }
-
-    handleChangeFirstName = (event) => {
-        this.setState({
-            firstName: event.target.value
-        })
-    }
-
-    handleChangeLastName = (event) => {
-        this.setState({
-            lastName: event.target.value
-        })
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        alert('Click me')
-        console.log(">>>Check State input: ", this.state)
-    }
-
     render() {
         console.log(">>>Check props: ", this.props)
         let { name, age, address, arrJobs } = this.props
