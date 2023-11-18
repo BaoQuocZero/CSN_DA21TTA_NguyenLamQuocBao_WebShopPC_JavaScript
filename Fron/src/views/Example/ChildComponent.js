1fee73731d0eddaef35c07d0f859e7cf2c3914d0
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-18 12:06:47 +0700

    19

--- Fron/src/views/Example/ChildComponent.js
+++ Fron/src/views/Example/ChildComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Demo.scss'
 
 class ChildComponent extends React.Component {
     state = {
@@ -23,7 +24,10 @@ class ChildComponent extends React.Component {
             <>
                 {showJobs === false ?
                     <div>
-                        <button onClick={() => this.handleShowHide()}>Show</button>
+                        <button className="btn-show"
+                            onClick={() => this.handleShowHide()}>
+                            Show
+                        </button>
                     </div>
                     :
                     <>
