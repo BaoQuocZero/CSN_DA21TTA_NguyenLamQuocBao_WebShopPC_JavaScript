df3468bcc6a4eb7cf2aeca0f954151245e0ee52e
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-24 20:32:24 +0700

    10

--- Back/src/controller/homeController.js
+++ Back/src/controller/homeController.js
@@ -1,6 +1,28 @@
+import connection from "../configs/connectDB"
 
 let getHomePage = (req, res) => {
-    return res.render("index.ejs")
+
+    let data = []
+    // simple query
+    connection.query(
+        'SELECT * FROM `user`',
+        function (err, results, fields) {
+            console.log(">>> Check mysql")
+            console.log(results); // results contains rows returned by server
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    address: row.address,
+                    firstName: row.firstName,
+                    lastName: row.lastName
+                })
+            });
+            return res.render("index.ejs", { dataUser: data })
+        }
+    );
+
+
 }
 
 module.exports = {
--- Back/src/server.js
+++ Back/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import configViewEngine from "./configs/viewEngine.js"
 import initWebRoute from "./route/web.js"
+// import connection from "./configs/connectDB.js"
 
 require('dotenv').config();
 
