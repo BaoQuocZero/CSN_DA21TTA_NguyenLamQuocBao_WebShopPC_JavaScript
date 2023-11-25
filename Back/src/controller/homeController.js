02e4f5aed511b444fc1c2d648b0b3acf8da1c362
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-25 18:18:42 +0700

    13

--- Back/src/controller/homeController.js
+++ Back/src/controller/homeController.js
@@ -12,14 +12,38 @@ let getDetailPage = async (req, res) => {
 }
 
 let createNewUser = async (req, res) => {
-    console.log(">>>Check: ", req.body)
     let { firstName, lastName, email, address } = req.body;
-    let [user] = await pool.execute(`
+    await pool.execute(`
     INSERT INTO user (firstName , lastName, email, address) VALUES (?, ?, ?, ?)`,
         [firstName, lastName, email, address]);
     return res.redirect('/')
 }
 
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId
+    await pool.execute(`
+    DELETE FROM user WHERE id = ?`,
+        [userId]);
+    return res.redirect('/')
+}
+
+let getEditPage = async (req, res) => {
+    let id = req.params.id
+    let [user] = await pool.execute(`
+    SELECT * FROM user WHERE id = ?`,
+        [id]);
+    return res.render("update.ejs", { dataUser: user[0] })
+}
+
+let postUpdateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+
+    await pool.execute(`
+    UPDATE user SET firstName = ?, lastName = ?, email=?, address=? WHERE id = ?`,
+        [firstName, lastName, email, address, id]);
+    return res.redirect('/')
+}
+
 module.exports = {
-    getHomePage, getDetailPage, createNewUser
+    getHomePage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser
 }
--- Back/src/route/web.js
+++ Back/src/route/web.js
@@ -6,7 +6,12 @@ let router = express.Router()
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/detail/user/:id', homeController.getDetailPage);
+    router.get('/edit-user/:id', homeController.getEditPage);
+
     router.post('/create-new-user', homeController.createNewUser)
+    router.post('/delete-user', homeController.deleteUser)
+    router.post('/update-user', homeController.postUpdateUser)
+
 
     router.get('/about', (req, res) => {
         res.send(`I'm Eric`)
