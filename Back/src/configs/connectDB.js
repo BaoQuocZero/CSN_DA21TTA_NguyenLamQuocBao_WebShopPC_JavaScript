befee4bbd8a820196bf5f6f2ce68bcfaec192b17
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-11-26 17:28:26 +0700

    xampp 3306 and docker 3307

--- Back/src/configs/connectDB.js
+++ Back/src/configs/connectDB.js
@@ -1,10 +1,19 @@
 import mysql from 'mysql2/promise'
 
 // create the connection to database
+
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'ShopPC_data'
 });
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     port: 3307,
+//     user: 'root',
+//     password: '0000',
+//     database: 'ShopPC_data'
+// });
 
 export default pool;
