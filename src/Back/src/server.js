70355a5314a5cf56dd507e1c82d57082edb5ec03
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-01-04 17:48:32 +0700

    Bao cao

--- src/Back/src/server.js
+++ src/Back/src/server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 8081;
 
-const cors = require("cors");
+const cors = require('cors');
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 // const cors = require('cors');
